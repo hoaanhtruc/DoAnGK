8b43d3e33660c651ede9381fb279997ca0f4bb00
Tran Thi Huyen <cogaibebong238@yahoo.com>
2017-04-22 22:12:02 +0700

    Load san pham

---
+++ WebOnline/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebOnlineConnection;
+
+namespace WebOnline.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var db = new WebOnlineConnectionDB();
+            IEnumerable <SanPham> dssp = db.Query<SanPham>("SELECT * FROM SanPham");
+            return View(dssp);
+        }
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+    }
+}
---
+++ WebOnlineBUS/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebOnlineBUS
+{
+    public class Class1
+    {
+    }
+}
