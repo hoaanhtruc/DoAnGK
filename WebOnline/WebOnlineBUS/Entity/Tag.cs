03bbb19176560de4f12a86d9053720037f0f1d5f
Tran Thi Huyen <cogaibebong238@yahoo.com>
2017-04-23 17:22:57 +0700

    Layout Admin

---
+++ WebOnline/WebOnlineBUS/Entity/Tag.cs
@@ -0,0 +1,27 @@
+namespace WebOnlineBUS.Entity
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("Tag")]
+    public partial class Tag
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public Tag()
+        {
+            SanPhams = new HashSet<SanPham>();
+        }
+
+        public int ID { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Ten { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<SanPham> SanPhams { get; set; }
+    }
+}
